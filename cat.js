f9a2339d8e554200c12135ca30e67e771b7b2554
raison00 <felicia.macys@gmail.com>
2016-01-22 12:39:05 -0800

    fluffy

--- cat.js
+++ cat.js
@@ -12,10 +12,11 @@ var mrGrumpy = new Cat('Mr.Grumpy', 'persian long hair', 4, '"purrrrr--purrr-per
 var kalie = new Cat('Tiger', 'calico', 0,'"mew mew"','https://s-media-cache-ak0.pinimg.com/236x/08/67/99/0867998a8e927bb50133bb5d2cd25f83.jpg');
 var blackie = new Cat('Blackie','tabby',0, '"meeew"', 'http://placekitten.com/150/170');
 var milo = new Cat('Milo','orange tabby',0, '"an appropriate cat sound"', 'http://oddstuffmagazine.com/wp-content/uploads/2011/09/Small-Cat-580x574.png');
+var fluffy = new Cat('fluffy','fluffy',0, '" cat cat"', 'http://cdn.sheknows.com/articles/2013/08/white-fluffy-cat-12.jpg');
 
 
 // Cat Array
-var catArray = [mrGrumpy,kalie,blackie,milo];
+var catArray = [mrGrumpy,kalie,blackie,milo, fluffy];
 
 //Output to HTML
 for (i=0;i < catArray.length; i++) {
