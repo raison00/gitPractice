195341b45ad179c9980998d1929fa32b0164b8e5
raison00 <felicia.macys@gmail.com>
2016-01-22 12:32:59 -0800

    hampsters

--- hampster.js
+++ hampster.js
@@ -10,11 +10,12 @@ function Hamp (name, breed, age, sound, imgUrl) {
 }
 
 // hamp Instances
-var Chunky = new Hamp('Chunky', 'where is the hampster', 14, 'peel banana. peel, peel banana! squeek squeek','https://www.google.com/imgres?imgurl=http://www.fourpaws.org/images_subpagenav/images_diversions/images_jokes_photos_pages/WhatHampster.jpg&imgrefurl=http://www.fourpaws.org/pages/diversion_pages/jokes_photos/WhatHampster.html&h=324&w=432&tbnid=PCym4Yb9c5FuoM:&docid=pe8otWHkokhP9M&ei=qoCiVuyaJ8ae-wGfy5TIBA&tbm=isch&ved=0ahUKEwjsl6--kb7KAhVGzz4KHZ8lBUkQMwhrKC8wLw');
-var Emy = new Hamp('Emy','hampster mix', 7, 'woof woof!','https://www.google.com/imgres?imgurl=http://funny.desivalley.com/wp-content/uploads/2011/03/hampster-funny-cartoon.jpg&imgrefurl=http://funny.desivalley.com/category/hampster-funny-picture/page/2/&h=360&w=480&tbnid=HUDi7TmpWmOdZM:&docid=bGyY1S5AOQ3i9M&ei=qoCiVuyaJ8ae-wGfy5TIBA&tbm=isch&ved=0ahUKEwjsl6--kb7KAhVGzz4KHZ8lBUkQMwh5KD0wPQ' );
+var Chunky = new Hamp('swingy', 'swinging for hampsters', 14, 'schwwing','http://imgfave-chat-herokuapp-com.global.ssl.fastly.net/image_cache/1310427450927631.jpg');
+var Emy = new Hamp('chunk speed','hampster mix', 7, 'coming!','http://en.bcdn.biz/Images/2015/9/18/839623cf-dc01-4504-8a1e-f4867bf4cc83.jpg' );
+var Biscuit = new Hamp('biscuit','biscuit mix', 7, 'biscuit!','http://images1.fanpop.com/images/image_uploads/CUTE-hamsters-1140511_450_466.jpg' );
 
 
-// hamp Array
+// hampArray
 var hampArray = [Chunky, Emy];
 
 //Output to HTML
